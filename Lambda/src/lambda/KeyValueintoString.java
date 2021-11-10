dee1a3b286928f1418532253f3d4b42baf0c4214
Nithinsarvade <57656464+Nithinsarvade@users.noreply.github.com>
2021-11-10 13:39:29 +0530

    8th

---
+++ Lambda/src/lambda/KeyValueintoString.java
@@ -0,0 +1,31 @@
+package lambda;
+
+import java.security.KeyStore.Entry;
+import java.util.HashMap;
+
+public class KeyValueintoString {
+	public static void main(String[] args)
+	{
+		HashMap<Integer,String> hs=new HashMap<>();
+		StringBuilder sb=new StringBuilder();
+		hs.put(1, "ProjectManager");
+		hs.put(2, "TeamLead");
+		hs.put(3, "SeniorDeveloper");
+		hs.put(4, "JuniorDeveloper");
+		
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("The elements in the hashmap are:");
+		System.out.println(hs);
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println(" ");
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("After converting entire hashmap into a string, the result is:");
+		for (java.util.Map.Entry<Integer, String> m:hs.entrySet()) 
+		{
+			sb.append(m);
+		}
+		System.out.println(sb);
+		System.out.println("-----------------------------------------------------------------");
+	}
+
+}
---
+++ Lambda/src/lambda/OddLength.java
@@ -0,0 +1,33 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddLength {
+	{
+		List<String> li=new ArrayList<>();
+		li.add("sai");
+		li.add("nithin");
+		li.add("arjun");
+		li.add("sam");
+		
+		System.out.println("-------------------------------");
+		System.out.println("Elements in the list are:");
+		
+		for (String string : li) {
+			System.out.println(string);
+		}
+		System.out.println("-------------------------------");
+		System.out.println(" ");
+		
+		li.removeIf(a->(a.length()%2 !=0));
+		System.out.println("------------------------------------------");
+		System.out.println("Elements in the list with odd lengths are:");
+		
+		for (String string : li) {
+			System.out.println(string);
+		}
+		System.out.println("------------------------------------------");
+	}
+
+}
